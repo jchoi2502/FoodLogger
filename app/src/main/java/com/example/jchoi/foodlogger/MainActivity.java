cadc47955e075fb27def36b0db0ee4d8dd3fdb62
hyungiko <hyungiko@uci.edu>
2018-06-23 15:53:45 -0700

    Added main ui, and survey ui

--- app/src/main/java/com/example/jchoi/foodlogger/MainActivity.java
+++ app/src/main/java/com/example/jchoi/foodlogger/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.jchoi.foodlogger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+    }
+
+    public void init() {
+        findViewById(R.id.btn_google).setOnClickListener(this);
+        findViewById(R.id.btn_facebook).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_google:
+                goToGoogle();
+                break;
+            case R.id.btn_facebook:
+                goToFacebook();
+                break;
+        }
+
+    }
+
+    public void goToGoogle() {
+        Intent myIntent = new Intent(MainActivity.this, SurveyActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    public void goToFacebook() {
+        Intent myIntent = new Intent(MainActivity.this, SurveyActivity.class);
+        MainActivity.this.startActivity(myIntent);
+
     }
 }
---
+++ app/src/main/java/com/example/jchoi/foodlogger/SurveyActivity.java
@@ -0,0 +1,17 @@
+package com.example.jchoi.foodlogger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hyungiko on 6/23/18.
+ */
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.survey_main);
+    }
+}
